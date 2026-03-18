c3b9f5f9dc778f65e731201f01538af918eaa5b3
Shaurya <shauryarchs@gmail.com>
2026-03-17 21:47:39 -0700

    initial xcode project setup

---
+++ EmberSensor/EmberSensor/EmberSensorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmberSensorApp.swift
+//  EmberSensor
+//
+//  Created by Dheeraj Varshnay Varshnay on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct EmberSensorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
